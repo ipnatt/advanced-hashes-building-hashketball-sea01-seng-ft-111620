31cac989752bf68bd6b2368478691de43fa32a57
Irvin Natt <ipnatt@gmail.com>
2020-11-09 08:18:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,123 +2,123 @@ def game_hash
   {
   :home_team => {
     :home_name => "Brooklyn Nets",
-    :team_colors => ["Black", "White"],
+    :team_colors => [Black, White],
     :team_players => [
       {
-      :name => "Alan Anderson",
+      :name => Alan Anderson,
       :number => 0,
-      :shoe_size => "16",
-      :points => "22"
-      :rebounds => "12"
-      :assists => "12"
-      :steals => "3"
-      :blocks => "1"
-      :slam_dunks => "1"
+      :shoe_size => 16,
+      :points => 22
+      :rebounds => 12
+      :assists => 12
+      :steals => 3
+      :blocks => 1
+      :slam_dunks => 1
       },
       {
-      :name => "Reggie Evans",
-      :number => "30",
-      :shoe_size => "14",
-      :points => "12",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "12",
-      :blocks => "12",
-      :slam_dunks => "7"
+      :name => Reggie Evans,
+      :number => 30,
+      :shoe_size => 14,
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
       },
       {
-      :name => "Brook Lopez",
-      :number => "11",
-      :shoe_size => "17",
-      :points => "17",
-      :rebounds => "19",
-      :assists => "10",
-      :steals => "3",
-      :blocks => "1",
-      :slam_dunks => "15"
+      :name => Brook Lopez,
+      :number => 11,
+      :shoe_size => 17,
+      :points => 17,
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 15
       },
       {
-      :name => "Mason Plumlee",
-      :number => "1",
-      :shoe_size => "19",
-      :points => "26",
-      :rebounds => "11",
-      :assists => "6",
-      :steals => "3",
-      :blocks => "8",
-      :slam_dunks => "5"
+      :name => Mason Plumlee,
+      :number => 1,
+      :shoe_size => 19,
+      :points => 26,
+      :rebounds => 11,
+      :assists => 6,
+      :steals => 3,
+      :blocks => 8,
+      :slam_dunks => 5
       },
       {
-      :name => "Jason Terry",
-      :number => "31",
-      :shoe_size => "15",
-      :points => "19",
-      :rebounds => "2",
-      :assists => "2",
-      :steals => "4",
-      :blocks => "11",
-      :slam_dunks => "1"
+      :name => Jason Terry,
+      :number => 31,
+      :shoe_size => 15,
+      :points => 19,
+      :rebounds => 2,
+      :assists => 2,
+      :steals => 4,
+      :blocks => 11,
+      :slam_dunks => 1
       }
     ]
   },
   :away_team => {
-    :away_name => "Charlotte Hornets",
-    :team_colors => ["Turquoise", "Purple"],
+    :away_name => Charlotte Hornets,
+    :team_colors => [Turquoise, Purple],
     :team_players => [
       {
-      :name => "Jeff Adrien",
-      :number => "4",
-      :shoe_size => "18",
-      :points => "10",
-      :rebounds => "1",
-      :assists => "1",
-      :steals => "2",
-      :blocks => "7",
-      :slam_dunks => "2"
+      :name => Jeff Adrien,
+      :number => 4,
+      :shoe_size => 18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2
       },
       {
-      :name => "Bismack Biyombo",
-      :number => "0",
-      :shoe_size => "16",
-      :points => "12",
-      :rebounds => "4",
-      :assists => "7",
-      :steals => "22",
-      :blocks => "15",
-      :slam_dunks => "10"
+      :name => Bismack Biyombo,
+      :number => 0,
+      :shoe_size => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
       },
       {
-      :name => "DeSagna Diop",
-      :number => "2",
-      :shoe_size => "14",
-      :points => "24",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "4",
-      :blocks => "5",
-      :slam_dunks => "5"
+      :name => DeSagna Diop,
+      :number => 2,
+      :shoe_size => 14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
       },
       {
-      :name => "Ben Gordon",
-      :number => "8",
-      :shoe_size => "15",
-      :points => "33",
-      :rebounds => "3",
-      :assists => "2",
-      :steals => "1",
-      :blocks => "1",
-      :slam_dunks => "0"
+      :name => Ben Gordon,
+      :number => 8,
+      :shoe_size => 15,
+      :points => 33,
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
+      :slam_dunks => 0
       },
       {
-      :name => "Kemba Walker",
-      :number => "33",
-      :shoe_size => "15",
-      :points => "6",
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "7",
-      :blocks => "5",
-      :slam_dunks => "12"
+      :name => Kemba Walker,
+      :number => 33,
+      :shoe_size => 15,
+      :points => 6,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 7,
+      :blocks => 5,
+      :slam_dunks => 12
       }
     ]
   }
