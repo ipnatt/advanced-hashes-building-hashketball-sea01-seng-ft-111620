c32506aa6f56a552993499fcec23cf2bb5db220d
Irvin Natt <ipnatt@gmail.com>
2020-11-09 07:48:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,6 +4,9 @@ def game_hash =
       :home_name => "Brooklyn Nets",
       :team_colors => ["Black", "White"],
       :team_players => [
+        {
+          
+        }
         ]
       
       
