51a5f612c9a973e83e05a77f140add839174737b
Irvin Natt <ipnatt@gmail.com>
2020-11-09 08:02:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,16 +5,16 @@ def game_hash =
     :home_name => "Brooklyn Nets",
     :team_colors => ["Black", "White"],
     :team_players => [
-        {
-          :name => "Alan Anderson",
-          :number => "0",
-          :shoe_size => "16",
-          :points => "22"
-          :rebounds => "12"
-          :assists => "12"
-          :steals => "3"
-          :blocks => "1"
-          :slam_dunks => "1"
+      {
+        :name => "Alan Anderson",
+        :number => "0",
+        :shoe_size => "16",
+        :points => "22"
+        :rebounds => "12"
+        :assists => "12"
+        :steals => "3"
+        :blocks => "1"
+        :slam_dunks => "1"
         },
         {
           :name => "Reggie Evans",
