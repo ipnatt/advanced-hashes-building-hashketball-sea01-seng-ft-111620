267440432937dd9825bab1115cf7862a1c665e44
Irvin Natt <ipnatt@gmail.com>
2020-11-09 08:24:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   {
   :home_team => {
-    :home_name => "Brooklyn Nets",
+    :team_name => "Brooklyn Nets",
     :colors => ["Black, White"],
     :players => [
       {
@@ -62,7 +62,7 @@ def game_hash
     ]
   },
   :away_team => {
-    :away_name => "Charlotte Hornets",
+    :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
       {
