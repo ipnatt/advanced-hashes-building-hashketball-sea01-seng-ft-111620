a10a454f14a0cecd033a2bd44deaae53cc3905b9
Irvin Natt <ipnatt@gmail.com>
2020-11-09 07:59:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,10 +20,10 @@ def game_hash =
           :number => "30",
           :shoe_size => "14",
           :points => "12",
-          :rebounds => "12"
-          :assists => "12"
-          :steals => "12"
-          :blocks => "12"
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "12",
+          :blocks => "12",
           :slam_dunks => "7"
         },
         {
@@ -38,25 +38,25 @@ def game_hash =
           :slam_dunks => "15"
         },
         {
-          :name => "Alan Anderson",
-          :number => "0",
-          :shoe_size => "16",
-          :points => "22"
-          :rebounds => "12"
-          :assists => "12"
-          :steals => "3"
-          :blocks => "1"
-          :slam_dunks => "1"
+          :name => "Mason Plumlee",
+          :number => "1",
+          :shoe_size => "19",
+          :points => "26",
+          :rebounds => "11",
+          :assists => "6",
+          :steals => "3",
+          :blocks => "8",
+          :slam_dunks => "5"
         },
         {
-          :name => "Alan Anderson",
-          :number => "0",
-          :shoe_size => "16",
-          :points => "22"
-          :rebounds => "12"
-          :assists => "12"
-          :steals => "3"
-          :blocks => "1"
+          :name => "Jason Terry",
+          :number => "31",
+          :shoe_size => "15",
+          :points => "19",
+          :rebounds => "2",
+          :assists => "2",
+          :steals => "4",
+          :blocks => "11",
           :slam_dunks => "1"
         }
         ]
